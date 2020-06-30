357a8586573fba9e67e31c1642617e93d38ca289
jens-hedenskog <jens.hedenskog@gmail.com>
2020-06-30 16:02:07 +0200

    Dynamicallt adds spawners, one per column

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Board : MonoBehaviour {
     public Settings _Settings;
     public GameObject _BoardTilePrefab;
+    public GameObject _LinkerSpawnerPrefab;
+    private List<LinkerSpawner> _Spawners = new List<LinkerSpawner>();
 
     void Start() {
         PositionBoard();
         InstantiateBoardTiles();
+        InstantiateLinkerSpawners();
     }
 
     private void PositionBoard() {
@@ -30,6 +34,25 @@ public class Board : MonoBehaviour {
                 bgTile.transform.parent = gameObject.transform;
                 ++count;
             }
+        }
+    }
+
+    private void InstantiateLinkerSpawners() {
+        for (int spawnerColumn = 0; spawnerColumn < _Settings._BoardWidth; ++spawnerColumn) {
+            GameObject spawnerObject = Instantiate(
+                _LinkerSpawnerPrefab,
+                new Vector3(
+                    gameObject.transform.position.x + spawnerColumn * Layouts._BoardTileSize.x,
+                    gameObject.transform.position.y + _Settings._BoardHeight * Layouts._BoardTileSize.y,
+                    0
+                ),
+                Quaternion.identity
+            );
+            spawnerObject.name = "Spawner_" + spawnerColumn;
+            spawnerObject.transform.parent = gameObject.transform;
+            LinkerSpawner spawner = spawnerObject.GetComponent<LinkerSpawner>();
+            spawner._ColumnIndex = spawnerColumn;
+            _Spawners.Add(spawner);
         }
     }
 }
