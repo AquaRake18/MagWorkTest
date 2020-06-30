c100410c501e2824d7f3f1453817bdc741dbac5a
jens-hedenskog <jens.hedenskog@gmail.com>
2020-06-30 11:12:56 +0200

    Added color count to settings.

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -5,6 +5,8 @@ public class Settings : MonoBehaviour {
     public int _BoardWidth = 6;
     [Range(3,9)]
     public int _BoardHeight = 9;
+    [Range(2,5)]
+    public int _Colors = 5;
     public int _ClearScore = 1000;
     public int _Moves = 24;
 }
