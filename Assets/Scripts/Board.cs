f37e77e787dac198487178e16e57351944465349
jens-hedenskog <jens.hedenskog@gmail.com>
2020-06-30 12:17:47 +0200

    Generates a board of max size. Added Layouts to separate how to place things.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,4 +2,26 @@
 
 public class Board : MonoBehaviour {
     public Settings _Settings;
+    public GameObject _BoardTilePrefab;
+
+    void Start() {
+        Vector2 boardPos = Layouts.GetBoardPos(_Settings);
+        int count = 0;
+        for (int y = 0; y < _Settings._BoardHeight; ++y) {
+            for (int x = 0; x < _Settings._BoardWidth; ++x) {
+                GameObject obj = Instantiate(
+                    _BoardTilePrefab,
+                    new Vector3(
+                        boardPos.x + x * Layouts._BoardTileSize.x,
+                        boardPos.y + y * Layouts._BoardTileSize.y,
+                        0
+                    ),
+                    Quaternion.identity
+                );
+                obj.name = "Tile_" + count;
+                obj.transform.parent = gameObject.transform;
+                ++count;
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Layouts.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class Layouts {
+    public static Vector2 _BoardTileSize = new Vector2(.5f, .5f);
+
+    public static Vector2 GetBoardPos(Settings settings) {
+        return new Vector2();
+    }
+}
