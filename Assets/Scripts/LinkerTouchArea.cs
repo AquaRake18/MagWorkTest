6b1c2cde1dea0d404b9f9ff16cf4027b8b069777
jens-hedenskog <jens.hedenskog@gmail.com>
2020-07-01 07:31:44 +0200

    Moved direction to its own class

--- Assets/Scripts/LinkerTouchArea.cs
+++ Assets/Scripts/LinkerTouchArea.cs
@@ -4,6 +4,7 @@ public class LinkerTouchArea : MonoBehaviour {
     private Vector3 _FirstTouchPosition;
     private Vector3 _FinalTouchPosition;
     public float _Angle;
+    public EDirection _Direction;
 
     void OnMouseDown() {
         _FirstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -19,22 +20,6 @@ public class LinkerTouchArea : MonoBehaviour {
             _FinalTouchPosition.y - _FirstTouchPosition.y,
             _FinalTouchPosition.x - _FirstTouchPosition.x
         ) * 180 / Mathf.PI;
-        if (_Angle > -22.5f && _Angle <= 22.5f) {
-            Debug.Log("East");
-        } else if (_Angle > 22.5f && _Angle <= 67.5f) {
-            Debug.Log("North East");
-        } else if (_Angle > 67.5f && _Angle <= 112.5f) {
-            Debug.Log("North");
-        } else if (_Angle > 112.5f && _Angle <= 157.5f) {
-            Debug.Log("North West");
-        } else if (_Angle > 157.5f || _Angle < -157.5f) {
-            Debug.Log("West");
-        } else if (_Angle < -112.5f && _Angle >= -157.5f) {
-            Debug.Log("South West");
-        } else if (_Angle < -67.5f && _Angle >= -112.5f) {
-            Debug.Log("South");
-        } else if (_Angle < -22.5f && _Angle >= -67.5f) {
-            Debug.Log("South East");
-        }
+        _Direction = Direction.AngleToDirection(_Angle);
     }
 }
