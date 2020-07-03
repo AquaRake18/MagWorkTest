e33f6b57f2650f0bdf96be621c52068e96487118
jens-hedenskog <jens.hedenskog@gmail.com>
2020-07-03 15:00:48 +0200

    Now we can share same levels!

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -3,26 +3,27 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 public static class SaveSystem {
-	private static string _Path = Application.persistentDataPath + "/levels.dat";
+	private static string _AssetsPath = Application.dataPath + "/LevelData/levels.dat";
+	private static string _SecurePath = Application.persistentDataPath + "/levels.dat";
 
 	public static void SaveLevels(LevelData[] storedLevels) {
 		BinaryFormatter formatter = new BinaryFormatter();
-		FileStream stream = new FileStream(_Path, FileMode.Create);
+		FileStream stream = new FileStream(_AssetsPath, FileMode.Create);
 
 		formatter.Serialize(stream, storedLevels);
 		stream.Close();
 	}
 
 	public static LevelData[] LoadLevels() {
-		if (File.Exists(_Path)) {
+		if (File.Exists(_AssetsPath)) {
 			BinaryFormatter formatter = new BinaryFormatter();
-			FileStream stream = new FileStream(_Path, FileMode.Open);
+			FileStream stream = new FileStream(_AssetsPath, FileMode.Open);
 
 			LevelData[] data = formatter.Deserialize(stream) as LevelData[];
 			stream.Close();
 			return data;
 		} else {
-			Debug.LogWarning("Save file not found at " + _Path);
+			Debug.LogWarning("Save file not found at " + _AssetsPath);
 		}
 		return null;
 	}
