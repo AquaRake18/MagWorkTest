3af9d34cc20d16faa6cc0bef8dd3818c4bd97413
jens-hedenskog <jens.hedenskog@gmail.com>
2020-07-04 20:35:42 +0200

    Added Googly Eyes that can be placed on existing Linker prefabs

---
+++ Assets/Scripts/RotateToMousePos.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotateToMousePos : MonoBehaviour {
+    void Update() {
+        Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 lookAt = mouseScreenPosition;
+        float angleRad = Mathf.Atan2(lookAt.y - transform.position.y, lookAt.x - transform.position.x);
+        float angleDeg = (180 / Mathf.PI) * angleRad;
+        transform.rotation = Quaternion.Euler(0, 0, angleDeg);
+    }
+}
