d29d587729edfc2961b792fc1eb47c1a58a3b53e
jens-hedenskog <jens.hedenskog@gmail.com>
2020-07-05 18:03:00 +0200

    Added a spinner to maybe use as backdrop for focused linker

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+    public Vector3 _RotationSpeed;
+
+    void Update() {
+        gameObject.transform.Rotate(_RotationSpeed);
+    }
+}
