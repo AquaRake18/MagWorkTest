f4bb794c6722a75cc4d0f9c140ff95ac5341cc20
jens-hedenskog <jens.hedenskog@gmail.com>
2020-07-14 20:20:27 +0200

    Added generic Singleton class to inherit.

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+    private static T _Instance;
+    public static T Instance {
+        get {
+            if (_Instance == null) {
+                _Instance = (T)FindObjectOfType(typeof(T));
+                if (_Instance == null) {
+                    GameObject singletonObject = new GameObject();
+                    _Instance = singletonObject.AddComponent<T>();
+                    singletonObject.name = typeof(T).ToString() + " (Singleton)";
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+            return _Instance;
+        }
+    }
+}
